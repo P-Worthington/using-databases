140803ca55ba47bbea2c2e153c4fe54b0d33051f
Paul Worthington <pworthington17@gmail.com>
2024-07-10 13:01:23 +0000

    changs

---
+++ test.py
@@ -0,0 +1,10 @@
+import key.py
+
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db = create_engine("postgresql:///test")
+base = declarative_base()
